35a14c7706bb4595f63c12f08591a76a79ecde7c
almirdantas1960 <almirdant@gmail.com>
2025-04-04 16:24:18 -0300

    atualizar

---
+++ esafio01/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="uft-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio01</title>
+</head>
+<body>
+    <header>
+        <h1>Informe um número</h1>
+    </header>
+    <section>
+        <form action="calc.php" method="get">
+            <label for="nom">Numero</label>
+            <input type="submit" value="Calcule">
+            <button type="button"></button>
+        </form>
+    </section>
+    
+</body>
+</html>
